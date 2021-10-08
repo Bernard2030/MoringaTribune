b6ec3d1154b3711353b02a37b0380d39f5212798
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-08 09:52:12 +0300

    push to heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn tribune.wsgi --log-file -
+web: gunicorn Tribune.wsgi --log-file -
--- Tribune/settings.py
+++ Tribune/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 
-# from pathlib import Path
+from pathlib import Path
 import os
 import django_heroku
 import dj_database_url
@@ -83,6 +83,18 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'tribune.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tribune',
+        'USER': 'moringa',
+        'PASSWORD':'Access'
+    }
+}
 
 
 
